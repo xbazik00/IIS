a6ce857c59fecbc324df835caba6e5a0cbcabb4b
DESKTOP-PN6IBPO\Jirka <cechak@inqool.cz>
2017-11-13 16:44:37 +0100

    game detail update

--- src/components/game/Info.js
+++ src/components/game/Info.js
@@ -1,32 +1,35 @@
 import React from "react";
+import { Card, CardText } from "react-md";
 
 import { formatTime } from "../../utils";
 
 const Info = ({ game }) => {
   return (
     <div className="flex-row flex-center">
-      <div className="flex-col">
-        <div className="flex-row">
-          <p className="row-label">Název:</p>
-          <p>{game.name}</p>
-        </div>
-        <div className="flex-row">
-          <p className="row-label">Žánr:</p>
-          <p>{game.genre}</p>
-        </div>
-        <div className="flex-row">
-          <p className="row-label">Datum vydání:</p>
-          <p>{formatTime(game.created)}</p>
-        </div>
-        <div className="flex-row">
-          <p className="row-label">Vydavatel:</p>
-          <p>{game.publisher}</p>
-        </div>
-        <div className="flex-row">
-          <p className="row-label">Módy:</p>
-          <p>{game.modes}</p>
-        </div>
-      </div>
+      <Card className="card-page">
+        <CardText>
+          <div className="flex-row">
+            <p className="row-label">Název:</p>
+            <p>{game.name}</p>
+          </div>
+          <div className="flex-row">
+            <p className="row-label">Žánr:</p>
+            <p>{game.genre}</p>
+          </div>
+          <div className="flex-row">
+            <p className="row-label">Datum vydání:</p>
+            <p>{formatTime(game.created)}</p>
+          </div>
+          <div className="flex-row">
+            <p className="row-label">Vydavatel:</p>
+            <p>{game.publisher}</p>
+          </div>
+          <div className="flex-row">
+            <p className="row-label">Módy:</p>
+            <p>{game.modes}</p>
+          </div>
+        </CardText>
+      </Card>
     </div>
   );
 };
