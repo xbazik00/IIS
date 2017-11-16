2e540d881886ece1a7f185c6507723f21620049a
DESKTOP-PN6IBPO\Jirka <cechak@inqool.cz>
2017-11-16 23:22:59 +0100

    utils update

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,5 +4,5 @@ import { indexOf } from "lodash";
 export const formatDate = date => format(date, "DD.MM.YYYY");
 export const formatTime = time => format(time, "DD.MM.YYYY HH:mm:ss");
 
-export const isAdmin = role => indexOf(role, "ROLE_ADMIN") > -1;
-export const isCoach = role => indexOf(role, "ROLE_COACH") > -1;
+export const isAdmin = role => role === "ADMIN";
+export const isCoach = role => role === "ROLE_COACH";
