6cf9387c9a78b3ebb6015a9c3a6f1ddaae235815
DESKTOP-PN6IBPO\Jirka <cechak@inqool.cz>
2017-11-25 14:00:18 +0100

    users filter fix

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -12,12 +12,14 @@ export const getUsers = () => async (dispatch, getState) => {
       const content = await response.json();
 
       const filterAll = getState().filter;
-      
+
       const list = !isEmpty(content.items)
         ? sortBy(
             filter(
               content.items,
-              c => get(c, filterAll.select).indexOf(filterAll.search) !== -1
+              c =>
+                get(c, filterAll.select) &&
+                get(c, filterAll.select).indexOf(filterAll.search) !== -1
             ),
             [filterAll.select]
           )
