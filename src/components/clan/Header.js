d562a4cca83693b8a717fe3ce3cf179aa4bc2e9e
DESKTOP-PN6IBPO\Jirka <cechak@inqool.cz>
2017-11-27 15:03:34 +0100

    clan, team header update

--- src/components/clan/Header.js
+++ src/components/clan/Header.js
@@ -1,26 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Button, Glyphicon } from "react-bootstrap";
+import { find } from "lodash";
 
 import { setDialog } from "../../actions/appActions";
 
 const Header = ({ history, clan, user, setDialog }) => (
   <div className="flex-row flex-space-between">
     <h2>{clan.name}</h2>
-    <Button
-      onClick={() => {
-        if (clan.boss === user.userName)
-          setDialog("DeleteClan", { tag: clan.tag });
-        else
-          setDialog("DeleteUserFromClan", {
-            tag: clan.tag,
-            userName: user.userName,
-            deleteMe: true
-          });
-      }}
-    >
-      <Glyphicon glyph="remove" />
-    </Button>
+    {clan && find(clan.users, u => u.userName === user.userName) ? (
+      <Button
+        onClick={() => {
+          if (clan.boss === user.userName)
+            setDialog("DeleteClan", { tag: clan.tag });
+          else
+            setDialog("DeleteUserFromClan", {
+              tag: clan.tag,
+              userName: user.userName,
+              deleteMe: true
+            });
+        }}
+      >
+        <Glyphicon glyph="remove" />
+      </Button>
+    ) : (
+      <div />
+    )}
   </div>
 );
 
--- src/components/team/Header.js
+++ src/components/team/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Button, Glyphicon } from "react-bootstrap";
+import { find } from "lodash";
 
 import { setDialog } from "../../actions/appActions";
 
 const Header = ({ history, user, setDialog, team, clan }) => (
   <div className="flex-row flex-space-between">
     <h2>{team.name}</h2>
-    {clan ? (
+    {team && find(team.users, u => u.userName === user.userName) ? (
       <Button
         onClick={() => {
           if (clan.boss === user.userName)
